8fc945e644af712a8c31efd3aeeabc4438eb3b82
trmttm <96355189+trmttm@users.noreply.github.com>
2021-12-27 21:10:04 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fm_calculator',
+    version='1.0.0',
+    packages=[],
+    url='',
+    license='',
+    author='Taro Yamaka',
+    author_email='t.yamaka.1213@gmail.com',
+    description=''
+)
